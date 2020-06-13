7ecda6c087f10fab172e1b866d669b8bc6d05098
unknown <sulakshgupta988@gmail.com>
2020-06-14 00:42:49 +0530

    draft code

---
+++ src/com/increff/employee/HelloWorld.java
@@ -0,0 +1,7 @@
+package com.increff.employee;
+
+public class HelloWorld {
+	public static void main(String args[]) {
+		   HelloWorld2.main(args);
+	}
+}
---
+++ src/com/increff/employee/HelloWorld2.java
@@ -0,0 +1,7 @@
+package com.increff.employee;
+
+public class HelloWorld2 {
+	public static void main(String[] args) {
+		HelloWorld3.main(args);
+	}
+}
